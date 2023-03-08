99c382250c9dd4f3479345d178e9a3e90b8bbcc2
davidlealo <davidlealo@gmail.com>
2023-03-08 20:23:26 -0300

    Levantar servidor, agregar contenido a repertorio.json

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const app = express()
+const fs = require('fs')
+
+app.listen(3000, ()=>{console.log('Servidor iniciado')})
+
+app.get('/', (req, res) =>{
+    res.sendFile(__dirname + '/index.html')
+})
+
+app.use(express.json())
+
+app.post('/canciones', (req, res) =>{
+    //Leer el archivo json
+    const file = fs.readFileSync('repertorio.json', 'utf8')
+    const fileParse = JSON.parse(file)
+
+    //Añadir nuevo registro
+    const newData = req.body
+    fileParse.push(newData)
+
+    //Sobre escribir el archivo
+    const writeFile = fs.writeFileSync('repertorio.json', JSON.stringify(fileParse))
+
+    //Notificar el ingreso del nuevo registro
+    res.send('Nuevo registro creado')
+} )
+
